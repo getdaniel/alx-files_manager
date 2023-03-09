770691be425cd395cd8fe9536f3f4e7aaa9e1124
Me <getdaniel>
2023-03-09 11:24:20 +0300

    remove promisfy

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,49 +1,52 @@
 import { v4 as uuidv4 } from 'uuid';
-import { promisify } from 'util';
 import sha1 from 'sha1';
-import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AuthController {
-  static async connect(req, res) {
-    const { authorization } = req.headers;
-    if (!authorization || !authorization.startsWith('Basic ')) {
+  static async getConnect(req, res) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    const credentials = Buffer.from(authorization.slice(6), 'base64').toString('ascii');
-    const [email, password] = credentials.split(':');
+    const encodedCredentials = authHeader.split(' ')[1];
+    const decodedCredentials = Buffer.from(encodedCredentials, 'base64').toString();
+    const [email, password] = decodedCredentials.split(':');
 
     const collection = dbClient.client.db().collection('users');
     const user = await collection.findOne({ email, password: sha1(password) });
-
     if (!user) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
     const token = uuidv4();
     const key = `auth_${token}`;
-    const value = user.id.toString();
-    const expireTime = 86400; // 24 hours in seconds
+    const expiresIn = 86400; // 24 hours in seconds
 
-    await promisify(redisClient.client.setex).bind(redisClient.client)(key, expireTime, value);
+    redisClient.setex(key, expiresIn, user.id);
 
     return res.status(200).json({ token });
   }
 
-  static async disconnect(req, res) {
+  static async getDisconnect(req, res) {
     const token = req.headers['x-token'];
+
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
     const key = `auth_${token}`;
-    const deleted = await promisify(redisClient.client.del).bind(redisClient.client)(key);
-    if (!deleted) {
+    const userId = await redisClient.get(key);
+
+    if (!userId) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    return res.status(204).end();
+    redisClient.del(key);
+
+    return res.status(204).send();
   }
 }
 
